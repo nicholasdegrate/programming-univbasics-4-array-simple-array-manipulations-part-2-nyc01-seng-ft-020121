596ad8a48e586a6bc38805ef34eeba6e4ecd5277
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-27 17:08:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array, new_arr)
+  return array.concat(new_arr)
+end
+
+def using_insert(array, element)
+  return array.insert(element)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, point)
+  return array.delete_at(point)
+end 
